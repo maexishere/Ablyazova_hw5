cb8142989bff39c48fadd0d783cb28987bb86b5d
maexishere <alexandra.ablyazova@student.csulb.edu>
2022-03-02 20:11:45 -0800

    5

--- js/ex1.js
+++ js/ex1.js
@@ -1,7 +1,23 @@
-const fang = new Dog("Fang", "boarhound", 75);
-console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
-console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+class Square {
+    constructor(sideS) {
+        this.side = sideS;
+    }
+    perimeter() {
+        return this.side * 4;
+    }
+    area() {
+        return this.side ** 2;
+    }
+    diagonal() {
+        return Math.sqrt(2 * this.side ** 2);
+    }
+}
 
-const snowy = new Dog("Snowy", "terrier", 22);
-console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
-console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
+const ws1 = new Square(2);
+console.log(`Square with side ${ws1.side} has perimeter of ${ws1.perimeter()}, area of ${ws1.area()}, and diagonal of ${ws1.diagonal()}`);
+
+const ws2 = new Square(3);
+console.log(`Square with side ${ws2.side} has perimeter of ${ws2.perimeter()}, area of ${ws2.area()}, and diagonal of ${ws2.diagonal()}`);
+
+const ws3 = new Square(4);
+console.log(`Square with side ${ws3.side} has perimeter of ${ws3.perimeter()}, area of ${ws3.area()}, and diagonal of ${ws3.diagonal()}`);
