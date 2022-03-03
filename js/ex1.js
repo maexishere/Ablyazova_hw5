7f0fd5b0f98fc326a754d97e2501a61f2723ba63
maexishere <alexandra.ablyazova@student.csulb.edu>
2022-03-02 19:23:16 -0800

    okey

---
+++ js/ex1.js
@@ -0,0 +1,7 @@
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+const snowy = new Dog("Snowy", "terrier", 22);
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
